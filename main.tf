227de1c9453d57b7475bdbe0703191bafa2903f8
Satyajit-Pawar <satyajitpawar2003@gmail.com>
2025-12-07 00:20:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,9 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
-  credentials = file(var.credentials_file)
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_instance" "vm_instance" {
