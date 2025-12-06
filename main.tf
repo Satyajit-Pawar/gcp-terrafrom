992ceba8f46a085ed0d20c2771575d6349ca7842
Satyajit-Pawar <satyajitpawar2003@gmail.com>
2025-12-07 00:09:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.5.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -9,31 +10,12 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
   project     = var.project_id
   region      = var.region
   zone        = var.zone
+  credentials = file(var.credentials_file)
 }
 
-# Create a VPC (optional)
-resource "google_compute_network" "vpc_network" {
-  name = "${var.project_id}-vpc"
-}
-
-# Create a firewall rule (allow SSH & HTTP)
-resource "google_compute_firewall" "default" {
-  name    = "vm-firewall"
-  network = google_compute_network.vpc_network.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "80", "8080"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
-# Create a GCP VM Instance
 resource "google_compute_instance" "vm_instance" {
   name         = var.instance_name
   machine_type = var.machine_type
@@ -42,17 +24,14 @@ resource "google_compute_instance" "vm_instance" {
   boot_disk {
     initialize_params {
       image = var.image
-      size  = var.disk_size
     }
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.self_link
-
-    access_config {
-      // Creates a public IP
-    }
+    network       = "default"
+    access_config {} # Assigns public IP (remove if you want private only)
   }
 
   metadata_startup_script = var.startup_script
 }
+
